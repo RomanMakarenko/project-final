e2fe83793baed88d857ef19d04392c892f45ac1e
roman.makarenko@nzz.ch <roman.makarenko@nzz.ch>
2024-05-01 23:32:26 +0300

    Add new functionality: adding tags to the task (REST API + implementation on the service)

--- src/main/java/com/javarush/jira/bugtracking/task/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/task/TaskService.java
@@ -21,6 +21,7 @@ import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import static com.javarush.jira.bugtracking.ObjectType.TASK;
 import static com.javarush.jira.bugtracking.task.TaskUtil.fillExtraFields;
@@ -139,5 +140,17 @@ public class TaskService {
         if (!userType.equals(possibleUserType)) {
             throw new DataConflictException(String.format(assign ? CANNOT_ASSIGN : CANNOT_UN_ASSIGN, userType, task.getStatusCode()));
         }
+    }
+
+    public void addTag(Long id, String tag) {
+        Task task = handler.getRepository().getExisted(id);
+        task.getTags().add(tag);
+        handler.update(task, id);
+    }
+
+    public void deleteTag(Long id, String tag) {
+        Task task = handler.getRepository().getExisted(id);
+        task.getTags().remove(tag);
+        handler.update(task, id);
     }
 }
